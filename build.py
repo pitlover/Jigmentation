95bc745054de3fa78764385244d9260c6e830b7f
jiyoungkim <pitlover@naver.com>
2022-06-20 19:50:06 +0900

    [ADD] build_model : dino, else

---
+++ build.py
@@ -0,0 +1,89 @@
+import torch
+from os.path import join
+from torchvision import models
+import wget
+import os
+import torch.nn as nn
+from model import DinoFeaturizer, LambdaLayer  # TODO need fix
+
+
+def build_model(opt: dict, data_dir: str):
+    # opt = opt["model"]
+    model_type = opt["name"].lower()
+
+    if model_type == "dino":
+        model = nn.Sequential(  # TODO need fix
+            DinoFeaturizer(20, opt),  # dim doesent matter
+            LambdaLayer(lambda p: p[0])
+        )
+
+    elif model_type == "robust_resnet50":
+        model = models.resnet50(pretrained=False)
+        model_file = join(data_dir, 'imagenet_l2_3_0.pt')
+        if not os.path.exists(model_file):
+            wget.download("http://6.869.csail.mit.edu/fa19/psets19/pset6/imagenet_l2_3_0.pt",
+                          model_file)
+        model_weights = torch.load(model_file)
+        model_weights_modified = {name.split('model.')[1]: value for name, value in model_weights['model'].items() if
+                                  'model' in name}
+        model.load_state_dict(model_weights_modified)
+        model = nn.Sequential(*list(model.children())[:-1])
+    elif model_type == "densecl":
+        model = models.resnet50(pretrained=False)
+        model_file = join(data_dir, 'densecl_r50_coco_1600ep.pth')
+        if not os.path.exists(model_file):
+            wget.download("https://cloudstor.aarnet.edu.au/plus/s/3GapXiWuVAzdKwJ/download",
+                          model_file)
+        model_weights = torch.load(model_file)
+        # model_weights_modified = {name.split('model.')[1]: value for name, value in model_weights['model'].items() if
+        #                          'model' in name}
+        model.load_state_dict(model_weights['state_dict'], strict=False)
+        model = nn.Sequential(*list(model.children())[:-1])
+    elif model_type == "resnet50":
+        model = models.resnet50(pretrained=True)
+        model = nn.Sequential(*list(model.children())[:-1])
+    elif model_type == "mocov2":
+        model = models.resnet50(pretrained=False)
+        model_file = join(data_dir, 'moco_v2_800ep_pretrain.pth.tar')
+        if not os.path.exists(model_file):
+            wget.download("https://dl.fbaipublicfiles.com/moco/moco_checkpoints/"
+                          "moco_v2_800ep/moco_v2_800ep_pretrain.pth.tar", model_file)
+        checkpoint = torch.load(model_file)
+        # rename moco pre-trained keys
+        state_dict = checkpoint['state_dict']
+        for k in list(state_dict.keys()):
+            # retain only encoder_q up to before the embedding layer
+            if k.startswith('module.encoder_q') and not k.startswith('module.encoder_q.fc'):
+                # remove prefix
+                state_dict[k[len("module.encoder_q."):]] = state_dict[k]
+            # delete renamed or unused k
+            del state_dict[k]
+        msg = model.load_state_dict(state_dict, strict=False)
+        assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
+        model = nn.Sequential(*list(model.children())[:-1])
+    elif model_type == "densenet121":
+        model = models.densenet121(pretrained=True)
+        model = nn.Sequential(*list(model.children())[:-1] + [nn.AdaptiveAvgPool2d((1, 1))])
+    elif model_type == "vgg11":
+        model = models.vgg11(pretrained=True)
+        model = nn.Sequential(*list(model.children())[:-1] + [nn.AdaptiveAvgPool2d((1, 1))])
+    else:
+        raise ValueError("No model: {} found".format(model_type))
+
+    if model_type != "dino":
+        model.eval()
+        model.cuda()
+
+    else:
+        bn_momentum = opt.get("bn_momentum", None)
+        if bn_momentum is not None:
+            for module_name, module in model.named_modules():
+                if isinstance(module, (nn.BatchNorm1d, nn.BatchNorm2d, nn.SyncBatchNorm)):
+                    module.momentum = bn_momentum
+        bn_eps = opt.get("bn_eps", None)
+        if bn_eps is not None:
+            for module_name, module in model.named_modules():
+                if isinstance(module, (nn.BatchNorm1d, nn.BatchNorm2d, nn.SyncBatchNorm)):
+                    module.eps = bn_eps
+
+    return model
