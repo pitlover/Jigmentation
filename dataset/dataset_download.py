7df91367d76a414f4289199197fec6fe7946311d
jiyoungkim <pitlover@naver.com>
2022-06-20 17:14:53 +0900

    [FIX] dataset_download.py

--- dataset/dataset_download.py
+++ dataset/dataset_download.py
@@ -3,20 +3,20 @@ import os
 
 
 def my_app() -> None:
-    pytorch_data_dir = "../Datasets/"
+    pytorch_data_dir = "/data/Datasets"
     dataset_names = [
         # "potsdam",
-        # "cityscapes",
-        "cocostuff"]
+        "cityscapes"]
+        # "cocostuff"]
     # "potsdamraw"]
     url_base = "https://marhamilresearch4.blob.core.windows.net/stego-public/pytorch_data/"
 
     os.makedirs(pytorch_data_dir, exist_ok=True)
     for dataset_name in dataset_names:
-        if (not os.path.exists(join(pytorch_data_dir, dataset_name))) or \
-                (not os.path.exists(join(pytorch_data_dir, dataset_name + ".zip"))):
+        if (not os.path.exists(os.path.join(pytorch_data_dir, dataset_name))) or \
+                (not os.path.exists(os.path.join(pytorch_data_dir, dataset_name + ".zip"))):
             print("\n Downloading {}".format(dataset_name))
-            wget.download(url_base + dataset_name + ".zip", join(pytorch_data_dir, dataset_name + ".zip"))
+            wget.download(url_base + dataset_name + ".zip", os.path.join(pytorch_data_dir, dataset_name + ".zip"))
         else:
             print("\n Found {}, skipping download".format(dataset_name))
 
