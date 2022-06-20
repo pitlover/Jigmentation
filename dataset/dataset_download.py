d10aebc8e8b82b7792f5b3e4e8f0e018f653302b
jiyoungkim <pitlover@naver.com>
2022-06-20 16:51:50 +0900

    [ADD] dataset_download.py

---
+++ dataset/dataset_download.py
@@ -0,0 +1,25 @@
+import wget
+import os
+
+
+def my_app() -> None:
+    pytorch_data_dir = "../Datasets/"
+    dataset_names = [
+        # "potsdam",
+        # "cityscapes",
+        "cocostuff"]
+    # "potsdamraw"]
+    url_base = "https://marhamilresearch4.blob.core.windows.net/stego-public/pytorch_data/"
+
+    os.makedirs(pytorch_data_dir, exist_ok=True)
+    for dataset_name in dataset_names:
+        if (not os.path.exists(join(pytorch_data_dir, dataset_name))) or \
+                (not os.path.exists(join(pytorch_data_dir, dataset_name + ".zip"))):
+            print("\n Downloading {}".format(dataset_name))
+            wget.download(url_base + dataset_name + ".zip", join(pytorch_data_dir, dataset_name + ".zip"))
+        else:
+            print("\n Found {}, skipping download".format(dataset_name))
+
+
+if __name__ == "__main__":
+    my_app()
